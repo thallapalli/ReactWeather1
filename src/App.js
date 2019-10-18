6041a74653c849f16b280811f6f9291eb037a7e0
Harsha M Shiva <HMS@Harshas-MacBook-Pro.local>
2019-10-17 23:42:15 -0500

    Handling Hooks in Functional Components

--- src/App.js
+++ src/App.js
@@ -1,46 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person.js';
 
-class App extends Component {
-state = {
-  persons: [
-    {name:'YYYY', age:32},
-    {name:'GGGG', age:35},
-    {name:'HHHH', age:37}
-  ]
-}
 
-changeNameHandler = () => {
+// imp diff between this and class based state handling.
+// Class based state handling will merge the state object 
+// and dont override other elements
+// Where as Function component state handling via Hooks will not merge.
+// This is very importatnt to remember.
+// Code sample is below 
+
+const App = props => {
+
+const [personState,setpersonState] = useState ({
+    persons: [
+      {name:'YYYY', age:32},
+      {name:'GGGG', age:35},
+      {name:'HHHH', age:37}
+    ]
+  });
+
+const changeNameHandler = () => {
  console.log('button Clicked');
  //Cannot Mutate value. DO NOT use this --> this.state.persons[].name = 'HUHU';
- this.setState({
+ setpersonState({
   persons: [
     {name:'BBBB', age:32},
     {name:'GGGG', age:35},
     {name:'HHHH', age:38}
   ]
+ });
 }
 
-
-
- )
-}
-
-
-
-  render() {
     return (
       <div className="App">
        <h1>My React Complete Guide</h1>
        <p>Building new is App is FUN</p>
-       <button onClick={this.changeNameHandler}>Change Name</button>
-       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Cricketer</Person>
-       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>retired Cricketer</Person>
+       <button onClick={changeNameHandler}>Change Name</button>
+       <Person name={personState.persons[0].name} age={personState.persons[0].age}/>
+       <Person name={personState.persons[1].name} age={personState.persons[1].age}>Cricketer</Person>
+       <Person name={personState.persons[2].name} age={personState.persons[1].age}>retired Cricketer</Person>
       </div>
     );
-  }
 }
 
 export default App;
---
+++ src/App_state.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person.js';
+
+class App extends Component {
+
+  state = {
+  persons: [
+    {name:'YYYY', age:32},
+    {name:'GGGG', age:35},
+    {name:'HHHH', age:37}
+  ]
+}
+
+changeNameHandler = () => {
+ console.log('button Clicked');
+ //Cannot Mutate value. DO NOT use this --> this.state.persons[].name = 'HUHU';
+ this.setState({
+  persons: [
+    {name:'BBBB', age:32},
+    {name:'GGGG', age:35},
+    {name:'HHHH', age:38}
+  ]
+}
+ )
+}
+  render() {
+    return (
+      <div className="App">
+       <h1>My React Complete Guide</h1>
+       <p>Building new is App is FUN</p>
+       <button onClick={this.changeNameHandler}>Change Name</button>
+       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Cricketer</Person>
+       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>retired Cricketer</Person>
+      </div>
+    );
+  }
+}
+
+export default App;
